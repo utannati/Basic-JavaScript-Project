fdf409033b6043b730dc06e4b01bf629d52819ab
Natnael Tekle <102483031+utannati@users.noreply.github.com>
2022-06-13 02:18:32 -0400

    New

---
+++ Basic JavaScript  Project/Project1_expretions_alert/JS/index.js
@@ -0,0 +1,10 @@
+var name = 'JavaScript'
+alert(name);
+function My_First_Function() {   //Defining a function and naming it 
+    var str = "Mussie told Natnael,\"Knock it Off, Natnael! or I will tell dad!\"<br>\"Eat my shorts!\ Natnael responded."
+    "\"be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss";  //Defining a variable and giving it a //string value
+var result = str.fontcolor("green");  //Using the fontcolor method on //str variable
+document.getElementById("Green_Text").innerHTML = result  //Putting the value  //of result into HTML element with "Green_Text" id 
+}
