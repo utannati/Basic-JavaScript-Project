8fbfd988c99763691520522f253b4f85d1852087
Natnael Tekle <102483031+utannati@users.noreply.github.com>
2022-06-26 19:22:10 -0400

    added file

---
+++ Project5_comparisons_type_coercion/index3.js
@@ -0,0 +1,42 @@
+var name = "NaN";
+alert(name);
+function my_function() {
+    document.getElementById("Test").innerHTML = isNaN('This is string');
+}
+
+function The_Function() {
+    document.getElementById("Check").innerHTML = isNaN('100');
+}
+
+document.write(3E320); document.write(-2E310);
+
+
+document.write(20 > 5); document.write(20 < 9);
+
+console.log(4 + 4);
+document.write("15" + 6);
+console.log[4 + 9][4 > 9];
+document.write(10 == 15);
+document.write(10 ==10);
+
+X = 20;
+Y = 20;
+document.write(X === Y);
+
+X = 30;
+Y = "30";
+document.write(X === Y);
+
+A = "Magnus";
+B = "Magnus";
+document.write(A === B);
+document.write(5 > 2 && 10 > 4);
+document.write(5 >2 || 10 > 4);
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(20 > 10);
+}
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(5 > 10);
+}
